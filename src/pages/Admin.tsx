b5fc2ad482355e73c151ec0459ca240f107ccce3
KoelschGOAT <luca.duepmann@gmail.com>
2023-02-16 07:04:02 +0100

    Fixed Screen height bug

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -11,12 +11,12 @@ import Stat from "../components/ui/Stat";
 import { stats } from "../services/types";
 const links = [
   {
-    link: "/clients/new",
+    link: "/admin/clients/new",
     label: "Client",
     icon: <RiComputerLine size={"1.5em"} />,
   },
   {
-    link: "/videos/new",
+    link: "/admin/videos/new",
     label: "Video",
     icon: <TfiVideoClapper size={"1.5em"} />,
   },
@@ -30,12 +30,12 @@ export const Admin = () => {
   const bg = isDarkMode ? "bg-dark-primary" : "bg-light-primary";
   return (
     <>
-      <div className={`flex h-screen ${bg}`}>
+      <div className={`flex h-full ${bg}`}>
         {" "}
         <Sidebar />
         <main className="flex w-screen justify-center">
           <div className="w-[80%]">
-            {location.pathname === "/" && (
+            {location.pathname === "/admin" && (
               <>
                 <Container
                   margin="mt-5"
@@ -64,13 +64,13 @@ export const Admin = () => {
                     Count={data.clients}
                     Icon={<RiComputerLine size={"2em"} />}
                     Title="Clients"
-                    to="/clients"
+                    to="/admin/clients"
                   />
                   <Stat
                     Count={data.videos}
                     Icon={<TfiVideoClapper size={"2em"} />}
                     Title="Videos"
-                    to="/videos"
+                    to="/admin/videos"
                   />
                 </div>
               </>
